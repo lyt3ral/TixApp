2296101e61cb5d43b1d881114d6f6f107ba331a5
lyt3ral <saisabarishmail@gmail.com>
2023-04-19 18:21:26 +0530

    Working on statistics and movie detail fetching

--- website/views.py
+++ website/views.py
@@ -5,18 +5,15 @@ from . import db
 import json
 from functools import wraps
 from datetime import datetime
+from dotenv import load_dotenv
+import os
+import requests
+import json
+
 
 views = Blueprint('views', __name__)
-
-# def admin_login_required(func):
-#     @wraps(func)
-#     def decorated_view(*args, **kwargs):
-#         if not current_user.is_authenticated or not current_user.is_admin:
-#             abort(403)  # return a 403 Forbidden error if the user is not an admin
-#         return func(*args, **kwargs)
-#     return login_required(decorated_view)
-
-
+load_dotenv()
+OMDB_API_KEY = os.getenv('OMDB_API_KEY')
 
 @views.route('/', methods=['GET', 'POST'])
 @login_required
@@ -71,7 +68,7 @@ def book_movie(movie_id):
     # print(movie,shows[0])
     return render_template("book-movie.html", user=current_user, shows=shows, movie=movie)
 
-@views.route('/bookings', methods=['GET'])
+@views.route('/bookings', methods=['GET,POST'])
 @login_required
 def bookings():
     tickets = Ticket.query.filter_by(user_id=current_user.id)
@@ -80,44 +77,36 @@ def bookings():
     print(venues)
     return render_template("bookings.html", user=current_user, tickets=tickets, shows=shows, venues=venues)
 
-# @views.route('/ticketnew', methods=['GET','POST'])
-# @login_required
-# def ticket_new():
-#     if request.method == "POST":
-#         print(request.form)
+@views.route('/testapi/', methods=['GET','POST'])
+def testapi():
+    
+    if request.method == 'POST':
+        formdata = request.form
+        movieTitle = formdata['movieTitle']
+        url = 'http://www.omdbapi.com/?t={}&plot=full&apikey={}'.format(movieTitle, OMDB_API_KEY)
+        response = requests.get(url)
+        Data = response.json()
+        # Data = json.loads(data)
+        return render_template('testapi.html', user=current_user, moviesuccess="True", title=Data['Title'], rating=Data['imdbRating'], poster=Data['Poster'])
 
-#     ticket = Ticket.query.all()
-#     return render_template("ticket.html", user=current_user, ticket=ticket)
+    return render_template('testapi.html', user=current_user)
 
-# @views.route('/ticketnew/add', methods=['GET','POST'])
-# @login_required
-# def ticket_new_add():
-#     if request.method == "POST":
-#         data=request.form
-#         tk=Ticket(user_id=data.get('user'), show_id=data.get('show'), venue_id=data.get('venue'))
-#         db.session.add(tk)
-#         db.session.commit()
-#     return render_template("ticketadd.html", user=current_user)
+# @views.route('/addmovie/', methods=['GET','POST'])
+# def addmovie():
+#     if request.method == 'POST':
+#         formdata = request.form
+#         movieTitle = formdata['movieTitle']
+#         url = 'http://www.omdbapi.com/?t={}&plot=full&apikey={}'.format(movieTitle, OMDB_API_KEY)
+#         response = requests.get(url)
+#         Data = response.json()
+#         # Data = json.loads(data)
+#         # new_movie = Movie(title=Data['Title'], rating=Data['imdbRating'], poster=Data['Poster'])
+#         # db.session.add(new_movie)
+#         # db.session.commit()
+#         return render_template('addmovie.html', user=current_user, moviesuccess="True", title=Data['Title'], rating=Data['imdbRating'], poster=Data['Poster'])
 
-# @views.route('/book/<int:show_id>', methods=['GET','POST'])
-# @login_required
-# def book_ticket():
+#     return render_template('addmovie.html', user=current_user)
 
-
-# @views.route('/admin' , methods=['GET', 'POST'])
-# @admin_login_required
-# def admin():
-#     return render_template("admin.html", user=current_user)
-
-
-# @views.route('/delete-note', methods=['POST'])
-# def delete_note():  
-#     note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
-#     noteId = note['noteId']
-#     note = Note.query.get(noteId)
-#     if note:
-#         if note.user_id == current_user.id:
-#             db.session.delete(note)
-#             db.session.commit()
-
-#     return jsonify({})
+@views.route('/chart/', methods=['GET'])
+def chart():
+    return render_template('chart.html', user=current_user)
